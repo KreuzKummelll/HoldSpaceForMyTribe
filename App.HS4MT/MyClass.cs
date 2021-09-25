77b603f1ddaff05d4c146559624e3de631f94b11
Dicke Spargel <a.mclane@icloud.com>
2021-09-24 19:41:40 -0700

    The beginnings of a dynamically linked library

---
+++ App.HS4MT/MyClass.cs
@@ -0,0 +1,12 @@
+﻿// Created by Andrew McLane
+// 10thmagicbridge@gmail.com
+using System;
+namespace App.HS4MT
+{
+    public class MyClass
+    {
+        public MyClass()
+        {
+        }
+    }
+}
